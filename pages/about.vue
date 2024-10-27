3630bae86ea641514294615661b59e57e8722180
Abenezer Seifu <abenezerseifu123@gmail.com>
2024-10-27 19:25:20 +0300

    initial setup

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum itaque qui necessitatibus similique, animi error quam aspernatur minima, repudiandae delectus pariatur ullam. Excepturi laborum ratione cum saepe cumque debitis assumenda?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum itaque qui necessitatibus similique, animi error quam aspernatur minima, repudiandae delectus pariatur ullam. Excepturi laborum ratione cum saepe cumque debitis assumenda?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus ullam enim neque atque maiores vel beatae animi vitae consequuntur. Nulla aliquid optio repellendus modi est odio iste vero, quibusdam minus?</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
